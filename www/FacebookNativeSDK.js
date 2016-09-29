5824e5c58daef9b4030be2a3372a8c678f67224b
Antoine KNOCKAERT <antoine@yper.fr>
2016-09-29 17:45:36 +0200

    Reset to a basic project

--- www/FacebookNativeSDK.js
+++ www/FacebookNativeSDK.js
@@ -3,6 +3,7 @@ var exec = require("cordova/exec");
 var FacebookNativeSDK = {
 
   activateApp: function (s, f) {
+    console.log("CALLED !!!");
     exec(s, f, "FacebookNativeSDK", "activateApp", []);
   }
 
