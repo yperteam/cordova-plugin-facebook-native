498db97760eb09c744ab97d137813e71770b0a2e
alexis.gomes19@gmail.com <alexis.gomes19@gmail.com>
2016-10-10 17:06:41 +0200

    wip plugin

--- platforms/android/src/FacebookNativeSDK.java
+++ platforms/android/src/FacebookNativeSDK.java
@@ -2,9 +2,11 @@ package org.apache.cordova.plugin;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import android.R;
+import android.content.Context;
+import android.content.DialogInterface;
 
 public class FacebookNativeSDK extends CordovaPlugin {
 
