97f7385475d633ff4aec3a010224fc29767af754
alexis.gomes19@gmail.com <alexis.gomes19@gmail.com>
2016-10-10 14:05:49 +0200

    wip plugin

--- www/FacebookNativeSDK.js
+++ www/FacebookNativeSDK.js
@@ -1,14 +1,20 @@
-var exec = require("cordova/exec");
+//var exec = require("cordova/exec");
 
-var FacebookNativeSDK = {
+function FacebookNativeSDK(){}
+
+FacebookNativeSDK.activateApp = function(s, f){
+  console.log("CALLED !!!");
+  var onFail = function(){};
+  cordova.exec(s, f, "FacebookNativeSDK", "activateApp", []);
+};
+/*var FacebookNativeSDK = {
 
   activateApp: function (s, f) {
     console.log("CALLED !!!");
     exec(s, f, "FacebookNativeSDK", "activateApp", []);
   }
 
-}
+}*/
 
-window.FacebookNativeSDK = FacebookNativeSDK;
-
+//window.FacebookNativeSDK = FacebookNativeSDK;
 module.exports = FacebookNativeSDK;
